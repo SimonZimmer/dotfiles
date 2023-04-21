4596d0e37aed470349e89c05acb478200945bed8
SimonZimmer <simon@dear-reality.com>
2023-04-21 16:24:28 +0200

    remove autosync from treesitter

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,7 +1,7 @@
 require'nvim-treesitter.configs'.setup {
   ensure_installed = {'cpp', 'c', 'python', 'rust', 'lua'},
-  sync_install = true,
-  auto_install = true,
+  sync_install = false,
+  auto_install = false,
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = false,
