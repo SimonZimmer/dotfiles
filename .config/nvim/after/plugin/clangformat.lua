cced12ecae935738d943eb08fb893ec6c6240657
Simon Zimmermann <mail@simonzimmermann.de>
2024-01-31 17:42:13 +0100

    add new syntax

--- .config/nvim/after/plugin/clangformat.lua
+++ .config/nvim/after/plugin/clangformat.lua
@@ -1,12 +1,10 @@
--- Define the clangFormat function
-function clangFormat()
-  vim.cmd('silent! ClangFormat')
-end
+--function clangFormat()
+--  vim.cmd('silent! ClangFormat')
+--end
 
--- Set up an autocmd for BufWritePost event
-vim.cmd([[
-  augroup AutoClangFormat
-    autocmd!
-    autocmd BufWritePost *.c,*.cpp,*.h lua clangFormat()
-  augroup END
-]])
+--vim.cmd([[
+--  augroup AutoClangFormat
+--    autocmd!
+--    autocmd BufWritePost *.c,*.cpp,*.h lua clangFormat()
+-- augroup END
+--]])
--- .config/nvim/after/plugin/nul-ls.lua
+++ .config/nvim/after/plugin/nul-ls.lua
@@ -1,13 +1,10 @@
-local null_ls = require("null-ls")
+--local null_ls = require("null-ls")
 
-null_ls.setup({
-    sources = {
-        null_ls.builtins.formatting.black,
-        null_ls.builtins.diagnostics.flake8.with({extra_args = {"--max-line-length", "100"}}),
-    },
-})
+--null_ls.setup({
+--    sources = {
+--        null_ls.builtins.formatting.black,
+--        null_ls.builtins.diagnostics.flake8.with({extra_args = {"--max-line-length", "100"}}),
+--    },
+--})
 
-vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format, { remap = false })
-
--- Set up an autocmd for BufWritePost event
-vim.api.nvim_command("autocmd BufWritePost *.py lua vim.lsp.buf.format()")
+--vim.api.nvim_command("autocmd BufWritePost *.py lua vim.lsp.buf.format()")
