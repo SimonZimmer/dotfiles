bbb182df443cd88473947d00f954371eb47711fc
Simon Zimmermann <mail@simonzimmermann.de>
2026-09-14 13:42:05 +0200

    update agents

--- .config/nvim/lua/simonzimmer/options.lua
+++ .config/nvim/lua/simonzimmer/options.lua
@@ -16,20 +16,29 @@ vim.opt.incsearch = true
 
 vim.opt.scrolloff = 8
 
--- WSL has no WSLg/X server, so xclip can't reach the Windows clipboard.
--- win32yank.exe bridges the unnamedplus register to the real Windows clipboard.
-vim.g.clipboard = {
-	name = "win32yank",
-	copy = {
-		["+"] = "win32yank.exe -i --crlf",
-		["*"] = "win32yank.exe -i --crlf",
-	},
-	paste = {
-		["+"] = "win32yank.exe -o --lf",
-		["*"] = "win32yank.exe -o --lf",
-	},
-	cache_enabled = 0,
-}
+if vim.fn.has("mac") == 1 then
+	vim.g.clipboard = {
+		name = "macos",
+		copy = { ["+"] = "pbcopy", ["*"] = "pbcopy" },
+		paste = { ["+"] = "pbpaste", ["*"] = "pbpaste" },
+		cache_enabled = 0,
+	}
+elseif vim.fn.executable("win32yank.exe") == 1 then
+	-- WSL has no WSLg/X server, so xclip can't reach the Windows clipboard.
+	-- win32yank.exe bridges the unnamedplus register to the real Windows clipboard.
+	vim.g.clipboard = {
+		name = "win32yank",
+		copy = {
+			["+"] = "win32yank.exe -i --crlf",
+			["*"] = "win32yank.exe -i --crlf",
+		},
+		paste = {
+			["+"] = "win32yank.exe -o --lf",
+			["*"] = "win32yank.exe -o --lf",
+		},
+		cache_enabled = 0,
+	}
+end
 vim.opt.clipboard = 'unnamedplus'
 
 vim.opt.swapfile = false
